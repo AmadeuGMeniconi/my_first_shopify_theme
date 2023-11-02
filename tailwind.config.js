46fc8656ebc77795447ed28737ca1897697a9f42
AmadeuGMeniconi <amadmeni@gmail.com>
2023-11-02 01:51:13 +0000

    Attempting sublinks dropdown on header nav button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   content: [
-      './layout/*.liquid',
-      './sections/*.liquid',
-      './snippets/*.liquid',
-      './templates/*.liquid',
-      './templates/customers/*.liquid',
-   ],
-   theme: {
-      extend: {},
-   },
-   plugins: [],
+    content: [
+        './layout/*.liquid',
+        './sections/*.liquid',
+        './snippets/*.liquid',
+        './templates/*.liquid',
+        './templates/customers/*.liquid',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 };
